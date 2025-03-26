fd6b464303320559f41c08c6124e9708751a6bda
ming01.li <ming01.li@horizon.cc>
2025-03-26 17:32:21 +0800

    fix(camera): [X5-2974] Add deltatime for ae [3/3]
    Signed-off-by: ming01.li <ming01.li@horizon.cc>
    Change-Id: I383ce06a4e791f4277e43b37f46dafb262512f49

--- libcam/src/inc/develop/camera_sensor_dev.h
+++ libcam/src/inc/develop/camera_sensor_dev.h
@@ -142,6 +142,7 @@ typedef struct sensor_data {
 	uint32_t  exposure_time_min;
 	uint32_t  exposure_time_long_max;
 	uint32_t  exposure_time_init;
+	uint32_t  delta_time;
 	uint32_t  active_width;
 	uint32_t  active_height;
 #ifndef COMP_XJ3_CAM
